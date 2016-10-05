0d6dbe12e14ebfbebee127b67fbd766fc9bfb023
jinny <jinhoh21@naver.com>
2016-10-05 11:21:54 +0900

    test

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -14,59 +14,61 @@ import util.HttpRequestUtils;
 import util.IOUtils;
 
 public class HttpRequest {
-    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+	private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+	private Map<String, String> params;
+	private Map<String, String> headers = new HashMap<String, String>();
+	private RequestLine requestLine;
+	
+	public HttpRequest(InputStream is) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			String line = br.readLine();
+			log.debug("request Line : {}", line);
+			
+			if(line==null) {
+				return;
+			}
+			
+			while(!line.equals("")) {
+				line = br.readLine();
+				log.debug("Header : {}", line);
+				if(!line.equals("")) {
+					String[] tokens = line.split(":");
+					headers.put(tokens[0], tokens[1].trim());
+				}
+			}
+			
+			if(requestLine.isPost()) {
+				String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+				params = HttpRequestUtils.parseQueryString(body);
+			} else {
+				params = requestLine.getParams();
+			}
+			
+			
+		} catch(IOException e) {
+			log.error(e.getMessage());
+		}
+		
+		
+	}
+	
+	
+	public HttpMethod getMethod() {
+		return requestLine.getMethod();
+	}
+	
+	public String getPath() {
+		return requestLine.getPath();
+	}
+	
+	public String getHeader(String name) {
+		return headers.get(name);
+	}
+	
+	public String getParameter(String name) {
+		return params.get(name);
+	}
 
-    private Map<String, String> headers = new HashMap<String, String>();
 
-    private Map<String, String> params;
-
-    private RequestLine requestLine;
-
-    public HttpRequest(InputStream is) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            String line = br.readLine();
-            log.debug("request line : {}", line);
-
-            if (line == null) {
-                return;
-            }
-
-            requestLine = new RequestLine(line);
-
-            while (!line.equals("")) {
-                line = br.readLine();
-                log.debug("header : {}", line);
-                if (!line.equals("")) {
-                    String[] tokens = line.split(":");
-                    headers.put(tokens[0], tokens[1].trim());
-                }
-            }
-
-            if (requestLine.isPost()) {
-                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
-                params = HttpRequestUtils.parseQueryString(body);
-            } else {
-                params = requestLine.getParams();
-            }
-        } catch (IOException io) {
-            log.error(io.getMessage());
-        }
-    }
-
-    public HttpMethod getMethod() {
-        return requestLine.getMethod();
-    }
-
-    public String getPath() {
-        return requestLine.getPath();
-    }
-
-    public String getHeader(String name) {
-        return headers.get(name);
-    }
-
-    public String getParameter(String name) {
-        return params.get(name);
-    }
 }
--- src/main/java/http/RequestLine.java
+++ src/main/java/http/RequestLine.java
@@ -11,30 +11,38 @@ import util.HttpRequestUtils;
 public class RequestLine {
     private static final Logger log = LoggerFactory.getLogger(RequestLine.class);
 
+    //Enum , GET,POST로 구성됨.
     private HttpMethod method;
 
     private String path;
 
     private Map<String, String> params;
 
+    //POST /user/create HTTP/1.1
     public RequestLine(String requestLine) {
         log.debug("request line : {}", requestLine);
         String[] tokens = requestLine.split(" ");
         if (tokens.length != 3) {
             throw new IllegalArgumentException(requestLine + "이 형식에 맞지 않습니다.");
         }
+        //GET인지 POST인지 확인함.
         method = HttpMethod.valueOf(tokens[0]);
+        
+        //POST방식
         if (method.isPost()) {
             path = tokens[1];
             params = new HashMap<String, String>();
             return;
         }
 
+        //GET방식
         int index = tokens[1].indexOf("?");
+      //GET방식인데 뒤에 파라미터가 없는 경우.
         if (index == -1) {
             path = tokens[1];
             params = new HashMap<String, String>();
         } else {
+        	//GET방식인데 뒤에 파라미터가 있는 경우.
             path = tokens[1].substring(0, index);
             params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
         }
